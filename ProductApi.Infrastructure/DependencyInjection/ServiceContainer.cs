9aa7823ae38766a366912b1856f0c49b956b1239
Bhaskar365 <bhaskarjyotidas365@gmail.com>
2025-03-17 18:16:38 +0530

    application service container under dependency injection added

--- ProductApi.Infrastructure/DependencyInjection/ServiceContainer.cs
+++ ProductApi.Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿using eCommerce.SharedLibrary.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProductApi.Application.Interfaces;
+using ProductApi.Infrastructure.Data;
+using ProductApi.Infrastructure.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +15,26 @@ namespace ProductApi.Infrastructure.DependencyInjection
 {
     public static class ServiceContainer
     {
-        
+        public static IServiceCollection AddInfrastructureService(this IServiceCollection services, IConfiguration config) 
+        {
+            // add database connectivity
+            // add authentication scheme
+            SharedServiceContainer.AddSharedServices<ProductDbContext>(services, config, config["MySerilog:FineName"]!);
+
+            // create dependency injection (DI)
+            services.AddScoped<IProduct, ProductRepository>();
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseInfrastructurePolicy(this IApplicationBuilder app) 
+        {
+            // register middleware such as :
+            // global exception - handle external errors
+            // listen to only api gateway - block all outside API calls
+            SharedServiceContainer.UseSharedPolicies(app);
+
+            return app;
+        }
     }
 }
